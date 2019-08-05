165ac5723865b8b7aee03bf5f7963250d1f9174d
Adiel Matuf <matufadiel@gmail.com>
2019-08-05 21:18:49 +0300

    init project mvc

---
+++ smart_market/config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+    'url' : 'mongodb+srv://bigdata:bigdata@cluster0-lbatf.mongodb.net/test?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'useNewUrlParser': true
+     //Please replace your host file Here : 127.1.1.0 , Express is Collection Name (Database Name)
+};
